4bd4afcb1f252bfe1c06b5bd366e5372c21eb8c3
unknown <igboekwulusifranklin@gmail.com>
2021-01-08 01:57:20 +0100

    Latest commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+
 $(document).ready(function(){
     $('.navbar a').on('click', function(e){
         if(this.hash !== ''){
@@ -6,7 +15,14 @@ $(document).ready(function(){
             const hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800);
+            }, "slow");
         }
     }) 
+// $("#back2Top").click(function(event) {
+//     event.preventDefault();
+//     $("html, body").animate({ 
+//         scrollTop: 0 }, "slow");
+//     return false;
+// });
+
 });
